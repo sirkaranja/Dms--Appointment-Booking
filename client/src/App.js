053aff928e2581d8b0d73087b09c59069532113f
Daniel Karanja <sirkaranja12@gmail.com>
2023-08-22 02:09:42 +0300

    assets folder with logo

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import CalendarComponent from '../src/Components/Calendar/CalendarComponent'
 import Login from './Components/Authentication/Login'
+import Admin from './Components/Dashboard/Admin'
 
 function App() {
   return (
     <>
-    <Login />
+    {/* <Login /> */}
+    <Admin />
     {/* <CalendarComponent />*/}
     </> 
     
