fa5a1e0af583ece76fcdb407bd5ceb18387afe69
Daniel Karanja <sirkaranja12@gmail.com>
2023-08-23 23:51:54 +0300

    updated models with role based

--- Server/app.py
+++ Server/app.py
@@ -1,20 +1,16 @@
-from flask import Flask, render_template, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
-from flask_security import Security, SQLAlchemyUserDatastore, login_required, roles_required
-from flask_login import LoginManager, current_user
+from flask import Flask
 from flask_migrate import Migrate
-from models import db, User,Role, Appointment
-from datetime import datetime
+from models import db, User, Role, Appointment
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'appointmentsystem'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///appointment.db'
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///appointments.db' 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db.init_app(app)  # Initialize the database first
 
+migrate = Migrate(app, db)  # Then set up the migrations
 
-
+# Define routes and views here
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(port=5555)
--- Server/models.py
+++ Server/models.py
@@ -1,50 +1,129 @@
 from flask import Flask
-from flask_login import UserMixin
-from flask_security import RoleMixin
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import CheckConstraint
+from sqlalchemy.schema import CheckConstraint 
+from datetime import date, datetime
+from faker import Faker
+import random
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///appointment.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///appointments.db' 
 db = SQLAlchemy(app)
+fake = Faker()
 
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False, unique=True)
 
-roles_users = db.Table('roles_users',
-    db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
-    db.Column('role_id', db.Integer(), db.ForeignKey('role.id'))
-)
-
-class User(db.Model, UserMixin):
-    __tablename__ = 'user'
-    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    email = db.Column(db.String, unique=True)
-    password = db.Column(db.String(255), nullable=False, server_default='')
-    active = db.Column(db.Boolean())
-    roles = db.relationship('Role', secondary=roles_users, backref='roled')
-
-class Role(db.Model, RoleMixin):
-    __tablename__ = 'role'
-    id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(80), unique=True)
+class User(db.Model):
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'), nullable=False)
+    
+    role = db.relationship("Role", back_populates="users")
 
 class Appointment(db.Model):
     __tablename__ = 'appointments'
-    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    title = db.Column(db.String)
-    appoint_type = db.Column(db.String)
-    appointment_date = db.Column(db.Date)
-    appointment_time = db.Column(db.Time)
-    status = db.Column(db.String)
-    rescheduled_date = db.Column(db.Date)
-    rescheduled_time = db.Column(db.Time)
+    
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    description = db.Column(db.String)
+    date = db.Column(db.Date, nullable=False)
+    category = db.Column(db.String, nullable=False)
+    time = db.Column(db.String, nullable=False)
+    status = db.Column(db.String, nullable=False)
+    phone_number = db.Column(db.String)
+    
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    
+    user = db.relationship("User", back_populates="appointments")
     
     __table_args__ = (
-        CheckConstraint(
-            appoint_type.in_(['Departmental Updates', 'Client Consultation', 'Business', 'Staff Appointment', 'Conflict Resolution']),
-            name='appoint_type_check'
-        ),
-        CheckConstraint(
-            status.in_(['Approved', 'Rejected', 'Rescheduled']),
-            name='status_check'
-        )
+        CheckConstraint("status IN ('Approved', 'Rejected', 'Rescheduled', 'Referred')", name="check_status"),
+        CheckConstraint("category IN ('Departmental Updates', 'Client Consultation', 'Business', 'Staff Appointment', 'Conflict Resolution')", name="check_category"),
     )
+
+User.appointments = db.relationship("Appointment", order_by=Appointment.id, back_populates="user")
+Role.users = db.relationship("User", order_by=User.id, back_populates="role")
+# ... (other imports)
+
+# Create sample instances
+def create_sample_instances():
+    with app.app_context():
+        admin_role = Role(name='Admin')
+        secretary_role = Role(name='Secretary')
+        manager_role = Role(name='Manager')
+        
+        db.session.add_all([admin_role, secretary_role, manager_role])
+        db.session.commit()
+
+        admin_user = User(
+            name='Admin User',
+            email='admin@example.com',
+            password='adminpass',
+            role=admin_role
+        )
+
+        secretary_user = User(
+            name='Secretary User',
+            email='secretary@example.com',
+            password='secretpass',
+            role=secretary_role
+        )
+
+        manager_user = User(
+            name='Manager User',
+            email='manager@example.com',
+            password='managerpass',
+            role=manager_role
+        )
+
+        db.session.add_all([admin_user, secretary_user, manager_user])
+        db.session.commit()
+
+        appointment1 = Appointment(
+            title='Meeting with Client',
+            description='Discuss project details',
+            date=date(2023, 8, 25),
+            category='Client Consultation',
+            time='10:00 AM',
+            status='Approved',
+            phone_number='123-456-7890',
+            user=admin_user
+        )
+
+        appointment2 = Appointment(
+            title='John Felix Meeting',
+            description='Discuss updates',
+            date=date(2023, 8, 25),
+            category='Staff Appointment',
+            time='4:00 PM',
+            status='Rejected',
+            phone_number='112-654-3210',
+            user=manager_user
+        )
+        appointment3 = Appointment(
+            title='Team Meeting',
+            description='Discuss project updates',
+            date=date(2023, 8, 26),
+            category='Departmental Updates',
+            time='3:00 PM',
+            status='Approved',
+            phone_number='987-654-3210',
+            user=manager_user
+        )
+      
+        db.session.add_all([appointment1, appointment2,appointment3])
+        db.session.commit()
+
+# ... (rest of the code)
+
+if __name__ == '__main__':
+    with app.app_context():
+        create_sample_instances()
+        print("Sample instances created successfully.")
+
