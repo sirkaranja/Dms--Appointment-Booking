f82c77eb62e5f1523a838d6774dc9e1c78facfa5
Daniel Karanja <sirkaranja12@gmail.com>
2023-08-31 15:17:20 +0300

    delete method for users

--- Server/app.py
+++ Server/app.py
@@ -16,21 +16,6 @@ migrate = Migrate(app, db)  # Then set up the migrations
 @app.route('/')
 def home():
     return "<h1>dashboard</h1>"
-# Define the route to get all users
-@app.route('/users', methods=['GET'])
-def get_all_users():
-    users = User.query.all()
-    user_list = []
-    for user in users:
-        user_data = {
-            'id': user.id,
-            'name': user.name,
-            'email': user.email,
-            'role': user.role_id
-        }
-        user_list.append(user_data)
-    return jsonify(user_list)
-
 # Define the route to get all appointments
 @app.route('/appointments', methods=['GET'])
 def get_all_appointments():
@@ -138,7 +123,7 @@ def get_category_counts():
         return jsonify({'error': str(e)}), 500
 
 
-
+#user methods
 #method for  adding new users 
 @app.route('/users', methods=['POST'])
 def add_user():
@@ -157,7 +142,35 @@ def add_user():
         db.session.rollback()
         return jsonify({'error': str(e)}), 500
     
+# Define the route to get all users
+@app.route('/users', methods=['GET'])
+def get_all_users():
+    users = User.query.all()
+    user_list = []
+    for user in users:
+        user_data = {
+            'id': user.id,
+            'name': user.name,
+            'email': user.email,
+            'role': user.role_id
+        }
+        user_list.append(user_data)
+    return jsonify(user_list)
 
+#delete user route
+@app.route('/users/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    try:
+        user= User.query.get(user_id)
+        if user:
+            db.session.delete(user)
+            db.session.commit()
+            return jsonify({'message': 'User deleted successfully'}), 200
+        else:
+            return jsonify({'message': "User not found"}), 400
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({'error': str(e)}), 500
 
 
 if __name__ == '__main__':
